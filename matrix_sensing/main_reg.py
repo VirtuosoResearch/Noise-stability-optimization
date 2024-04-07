ec9fa851179251644e2af0c961d88048d287eaec
hongyangsg <hongyang90@gmail.com>
2024-04-07 13:50:57 -0400

    update

--- matrix_sensing/main_reg.py
+++ matrix_sensing/main_reg.py
@@ -3,55 +3,40 @@ from matrix_sensing import *
 import numpy as np
 
 # Independent variables
-d = 100                          # Size of ground truth matrix
+d = 50                          # Size of ground truth matrix
 r = 5                           # Rank of ground truth matrix
-c = 5                          # For m = c*d*r
-is_classification = False#True        # Use this to control regression/classification
+c = 10                          # For m = c*d*r
 use_factorization = True        # Does the model use UU^T or just U
-max_epochs = 10000 #10*10**3
-learning_rate = 0.001
+max_epochs = 2000
+learning_rate = 0.0001
 
 # initialize variables, generate X, Ais, and labels y
+# initialize variables, generate X, Ais, and labels y
 m = c*d*r
-print("d, r, m:", d, r, m)
-print("Generating training data... ", end="")
-#X, U, D, V = make_low_rank_matrix(d, r)
-X = make_low_rank_matrix(d, r)
-Xnp = X.numpy()
-Ais = random_RIPs(X.shape[0], symmetric=True, count=m)
-ytrue = do_measurement(Ais, X, False)
-ytrue_squared_average = torch.sum(ytrue**2)
-print("Done.")
-print("Generating validation data... ", end="")
+U_true, X = make_low_rank_matrix(d, r)
+Ais = random_RIPs(X.shape[0], count=m)
+y_true = do_measurement(Ais, X, d)
 
-val_Ais = random_RIPs(X.shape[0], symmetric=True, count=m)
-val_ytrue = do_measurement(val_Ais, X, False)
-val_ytrue_squared_average = torch.sum(val_ytrue**2)
-print("Done.")
+val_Ais = random_RIPs(X.shape[0], count=m)
+val_ytrue = do_measurement(val_Ais, X, d)
 
 # Intialize model related variables and objects
-activate = is_classification
-error_fn = 'mse'
-alpha_fn = 1.00
-net = MatrixSensing(d, use_factorization=True, activate=False, error_function='mse', alpha_fn=alpha_fn)
-SGDoptimiser = torch.optim.SGD(net.parameters(), lr=learning_rate, weight_decay=0.0)
-
-# Training related variables
-# max_loss_acceptable = 10**-2
-# loss_improvement_tolerance = 10**-3
+init_scale = 1.00
+lr = 0.0025
 max_epochs_10_perc = 20
+U = np.random.randn(d, d) * init_scale
 
-print("Training for", max_epochs, "epochs with lr:", learning_rate)
+perturb = 0.01
+max_epochs = 10001
 for epoch in range(1, max_epochs):
-    yhat = train(net, Ais, ytrue, SGDoptimiser)
-    train_loss = mse_loss(ytrue, yhat)
+    U = train_NSO(Ais, y_true, U, lr, perturb, d, True)
+    y_pred = do_measurement(Ais, U @ U.T, d)
+    train_loss = mse_loss(y_true, y_pred)
 
     # Print stats during training
     if (epoch % max_epochs_10_perc == 0):
-        val_yhat = net.forward(val_Ais)
-        val_loss = mse_loss(val_yhat, val_ytrue)
+        val_yhat = do_measurement(val_Ais, U @ U.T, d)
+        val_loss = mse_loss(val_ytrue, val_yhat)
 
-        model_U = net.U.weight.t().detach().numpy()
-        actual_model_U = model_U @ model_U.T
-        diff = np.linalg.norm(Xnp - actual_model_U, 'fro') / np.linalg.norm(Xnp, 'fro')
+        diff = np.linalg.norm(X - U @ U.T, 'fro') / np.linalg.norm(X, 'fro')
         print("Epoch", epoch, "Training", train_loss, "Validation", val_loss, "Distance", diff)
--- matrix_sensing/matrix_sensing.py
+++ matrix_sensing/matrix_sensing.py
@@ -3,44 +3,33 @@ Matrix Sensing using PyTorch
 '''
 import numpy as np
 import numpy.linalg as npla
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-import torch.nn.functional as F
-from scipy.stats import ortho_group
-
-def sharper_sigmoid(x, sharpness=1):
-    return 1/(1+torch.exp(-sharpness*x))
 
 def make_low_rank_matrix(d, r):
     U = np.random.normal(0, 1, (d, r))
-    return torch.from_numpy(U).type(torch.DoubleTensor), torch.from_numpy(U @ U.T).type(torch.DoubleTensor)
+    return U, U @ U.T
 
 def random_RIPs(d, count):
     rip = np.random.normal(0, 1, size=(count, d, d))
     inds = range(count)
     rip[inds, :, :] = 0.5*(rip[inds, :, :] + np.transpose(rip, (0, 2, 1))[inds, :, :])
-    return torch.from_numpy(rip).type(torch.DoubleTensor)
+    return rip
 
-def batch_matrix_ip(Ais, X):
-    traces = torch.zeros(Ais.shape[0])
-    if ((len(Ais.shape) != len(X.shape)+1)
-        or len(X.shape) != 2):
-        print("Arguments should be of the form (batch_size, n, n) and (n, n).")
-        print("Received: {} and {}".format(Ais.shape, X.shape))
-        return traces
+def do_measurement(Ais, X, d):
+    reshaped_A = Ais.reshape(-1, d * d)
+    reshaped_X = X.reshape(d * d, 1)
+    return reshaped_A @ reshaped_X
+    #Ais.
+    #traces = torch.zeros(Ais.shape[0])
+    #if ((len(Ais.shape) != len(X.shape)+1)
+    #    or len(X.shape) != 2):
+    #    print("Arguments should be of the form (batch_size, n, n) and (n, n).")
+    #    print("Received: {} and {}".format(Ais.shape, X.shape))
+    #    return traces
 
-    m, d, _ = Ais.shape
-    Ais_reshape, X_reshape = Ais.view((m, d*d)), X.reshape(d*d, 1)
-    traces = torch.mm(Ais_reshape, X_reshape).view((m,))
+    #m, d, _ = Ais.shape
+    #Ais_reshape, X_reshape = Ais.view((m, d*d)), X.reshape(d*d, 1)
+    #traces = torch.mm(Ais_reshape.float(), X_reshape.float()).view((m,))
     return traces
-
-def do_measurement(Ais, X, do_sigmoid):
-    y = batch_matrix_ip(Ais, X)
-    if do_sigmoid:
-        y = sharper_sigmoid(y)
-        y = y > 0.5
-    return y.type(torch.DoubleTensor)
 
 def get_prob_label(Ais, X):
     y = batch_matrix_ip(Ais, X)
@@ -49,95 +38,34 @@ def get_prob_label(Ais, X):
     y_label = (probs < y).type(torch.DoubleTensor)
     return y_label
 
-class MatrixSensing(nn.Module):
-    '''
-    The Module to perform Matrix Sensing.
+def train_GD(Ais, y_true, U, lr, d):
+    grad = get_grad(Ais, y_true, U, d)
+    U = U - lr * grad
+    return U
 
-    Uses nn.Linear for U and accesses U.weight to introduce factorization
-    '''
-    def __init__(self, d, use_factorization=True, error_function='bce', alpha_fn=1.0): #1/d**2
-        super().__init__()
+def train_NSO(Ais, y_true, U, lr, perturb, d, use_negative = True):
+    V = np.random.normal(0, 1, (d, d)) * perturb
 
-        self.d = d              # matrix dimension
-        self.use_factorization = use_factorization  # use UU^T form?
-        self.alpha = 1.0 #alpha_fn
+    plus_grad  = get_grad(Ais, y_true, U + V, d)
+    if use_negative:
+        minus_grad = get_grad(Ais, y_true, U - V, d)
+    else:
+        minus_grad = plus_grad
 
-        print("#### Model settings ####")
-        print("Factorization :", self.use_factorization)
-        print("Error         :", error_function)
-        #print("alpha         :", self.alpha)
-        print("########")
+    U = U - lr * (plus_grad + minus_grad) / 2
+    return U
 
-        # proper init
-        self.U = nn.Linear(d, 5, bias=False)
-        self.U.weight.data = torch.t(alpha_fn).data.type(torch.DoubleTensor)
-        #self.U.weight.data = (0.01 * self.U.weight.data).type(torch.DoubleTensor)
 
-        # full rank init
-        #self.U = nn.Linear(d, d, bias=False)
-        #nn.init.eye_(self.U.weight)
-        #self.U.weight.data = self.U.weight.data.double()
-        #self.U.weight.data = 0.005 * self.U.weight.data
-        #print(3, self.U.weight.shape, self.U.weight.type())
+def get_grad(Ais, y_true, U, d):
+    y_pred = do_measurement(Ais, U @ U.T, d)
 
-        # which loss function to use.
-        if error_function=='logistic':
-            self.error_function = torch.nn.SoftMarginLoss()
-        elif error_function=='mse':
-            self.error_function = torch.nn.MSELoss()
-        elif error_function == 'bce':
-            self.error_function = torch.nn.BCEWithLogitsLoss()
-        else:
-            print("Unrec loss fn")
+    reshaped_A = Ais.reshape(-1, d * d)
+    convolved_A = np.diag((y_pred - y_true).flatten()) @ reshaped_A
+    convolved_A = np.sum(convolved_A, axis = 0) / len(y_true)
+    convolved_A = convolved_A.reshape(d, d)
 
-        # keep track of loss value using this
-        self.loss_value = torch.from_numpy(np.array([np.inf])).type(torch.DoubleTensor)
-        self.loss_value.requires_grad_(True)
-
-    def forward(self, Ai):
-        if self.use_factorization:
-            # Note that internally torch stores U already transposed.
-            # So we use U.weight.t() when we would want to use the matrix U
-            # and we would use U.weight when we want U^T.
-            yhat = do_measurement(Ai, torch.mm(self.U.weight.t(), self.U.weight), False)
-        else:
-            yhat = do_measurement(Ai, self.U.weight.t(), False)
-
-        return yhat
-
-def train(net, Ais, y_input, optimiser, project=False):
-    yhat = net.forward(Ais)
-
-    net.loss_value = net.error_function(yhat, y_input)
-    optimiser.zero_grad()
-    net.loss_value.backward()
-    optimiser.step()
-
-    # project
-    #if project:
-    #    net.U.weight.data = net.U.weight.data / torch.norm(net.U.weight.data)
-    return [yhat, net.loss_value]
-
-def train_noise(net, Ais, y_input, optimiser, noise_rate = 0.01):
-    #noise_Ai = Ais + noise_rate * torch.randn(Ais.shape).double()
-    yhat = net.forward(Ais)
-    ytrue = Variable(y_input, requires_grad=False)
-    #y_noise = torch.rand(y_input.shape) < noise_rate
-    #y_flip = (y_input + y_noise.double()) % 2
-    #y_flip = y_flip.double()
-
-    #net.loss_value = net.error_function(yhat, y_flip)
-    net.loss_value = net.error_function(yhat, ytrue)
-    optimiser.zero_grad()
-    net.loss_value.backward()
-    optimiser.step()
-    return yhat
-
-def binary_loss(ytrue, yhat):
-    y_binary = sharper_sigmoid(yhat)
-    y_binary = (y_binary > 0.5).type(torch.DoubleTensor)
-    return torch.mean(torch.abs(y_binary - ytrue)).item()
+    grad = convolved_A @ U
+    return grad
 
 def mse_loss(ytrue, yhat):
-    return torch.mean((yhat-ytrue)**2).item()/torch.mean(ytrue**2).item()
-
+    return np.mean((yhat - ytrue) ** 2) / np.mean(ytrue ** 2)
